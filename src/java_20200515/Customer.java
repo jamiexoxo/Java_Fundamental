a2cf6d05cd593fc198a779a6f2cae87aac241860
jamiexoxo <youona@naver.com>
2020-05-15 15:28:46 +0900

    2020-05-15-v1

---
+++ src/java_20200515/Customer.java
@@ -0,0 +1,13 @@
+package java_20200515;
+//Customer 클래스
+public class Customer {
+	//클래스의 멤버 변수 - instance 변수, static 변수, final 변수
+	//인스턴스 변수(name, email, phone, balance, isReleased)
+	//객체가 생성될 때만 만들어지는 변수
+	public String name;
+	public String email;
+	public String phone;
+	public double balance;
+	public boolean isReleased;
+	
+}
